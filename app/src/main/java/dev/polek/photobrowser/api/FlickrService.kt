274464cdf4ba49c4b574fc83dcc96cba7f2f1a88
Yury Polek <yury.polek@gmail.com>
2020-11-30 02:17:38 +0200

    remove unused import

--- app/src/main/java/dev/polek/photobrowser/api/FlickrService.kt
+++ app/src/main/java/dev/polek/photobrowser/api/FlickrService.kt
@@ -2,7 +2,6 @@ package dev.polek.photobrowser.api
 
 import androidx.annotation.IntRange
 import com.serjltt.moshi.adapters.Wrapped
-import dev.polek.photobrowser.model.Photo
 import retrofit2.http.GET
 import retrofit2.http.Query
 
